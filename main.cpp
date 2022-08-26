fcaafc9f538fee24400e0744b6250a4676902ae8
Erwin Coumans <erwin.coumans@gmail.com>
2022-08-25 21:00:15 -0700

    minimal example of using CUDA (cuda.so and cudart.so) without dynamic/static linking against CUDA libraries. This is using dlopen/dlsym. Note you can also do this without dlopen/dlsym using clang, to avoid cmake needing to link against dl
    https://github.com/llvm/llvm-project/commit/540a13652fda8b91b62b73fb9ae1e34879e8e36c
    This patch replaces uses of `dlopen` and `dlsym` with LLVM's support
    with `loadPermanentLibrary` and `getSymbolAddress`.
    
    Use example:
    
    void TinyOpenGL3App::init_cuda_interop(
    {
    
      unsigned int* cuda_dest_resource;
      GLuint shDrawTex;  // draws a texture
      struct cudaGraphicsResource* cuda_tex_result_resource;
      // get read-write access to OpenGL texture buffer
      GLuint tex_cudaResult;
      tex_cudaResult = cudaGraphicsGLRegisterImage(
          &cuda_tex_result_resource, renderTextureId, GL_TEXTURE_2D,
          cudaGraphicsMapFlagsReadOnly);  // cudaGraphicsMapFlagsNone);
      assert(tex_cudaResult == cudaSuccess);
    
      tex_cudaResult = cudaGraphicsMapResources(1, &cuda_tex_result_resource, NULL);
      assert(tex_cudaResult == cudaSuccess);
    
      // https://stackoverflow.com/questions/9406844/cudagraphicsresourcegetmappedpointer-returns-unknown-error
      // In other words, use GetMappedPointer for mapped buffer objects. Use
      // GetMappedArray for mapped texture objects.
      //
      // struct cudaGraphicsResource* cuda_resource = 0;
      cudaArray* texture_ptr;
      tex_cudaResult = cudaGraphicsSubResourceGetMappedArray(
          &texture_ptr, cuda_tex_result_resource, 0, 0);
    
      // size_t cuda_dev_ptr_size = 0;
      // tex_cudaResult = cudaGraphicsResourceGetMappedPointer(
      //         (void**)&texture_ptr, &cuda_dev_ptr_size,
      //         cuda_tex_result_resource);
      assert(tex_cudaResult == cudaSuccess);
    
      int num_texels = render_width * render_height;
      int num_values = num_texels * 4;
      int size_tex_data = sizeof(GLubyte) * num_values;
      // tex_cudaResult = cudaMemcpyToArray(texture_ptr, 0, 0, cuda_dest_resource,
      //                                   size_tex_data, cudaMemcpyDeviceToDevice);
      unsigned char* cpu_image = (unsigned char*)malloc(size_tex_data);
      tex_cudaResult = cudaMemcpyFromArray(
          cpu_image, texture_ptr, 0, 0, size_tex_data,
          cudaMemcpyDeviceToHost);  // cudaMemcpyDeviceToDevice);
    
      free(cpu_image);
    
      tex_cudaResult = cudaGraphicsUnmapResources(1, &cuda_tex_result_resource, 0);
      assert(tex_cudaResult == cudaSuccess);
    
    }

--- main.cpp
+++ main.cpp
@@ -1,7 +1,89 @@
 #include <iostream>
+#include <dlfcn.h>
 using namespace std;
+
+#define DYNAMIC_CUDA_PATH "/usr/lib/x86_64-linux-gnu/libcuda.so"
+#define DYNAMIC_CUDART_PATH "/usr/local/cuda/lib64/libcudart.so"
+
+enum TINY_CUDA_CODES
+{
+  CUDA_SUCCESS                              = 0,
+  CU_GET_PROC_ADDRESS_DEFAULT = 0,
+  cudaEnableDefault = 0,
+};
+
+// CUDA driver API functions.
+typedef struct cudaGraphicsResource* cudaGraphicsResource_t;
+typedef struct CUstream_st* cudaStream_t;
+typedef struct cudaArray_st* cudaArray_t;
+
+// see https://docs.nvidia.com/cuda/cuda-runtime-api/driver-vs-runtime-api.html#driver-vs-runtime-api
+// cuda driver (cuda.so)
+TINY_CUDA_CODES (*cuDriverGetVersion)(int *version);
+TINY_CUDA_CODES (*cuInit)(unsigned int flags);
+TINY_CUDA_CODES (*cuDeviceGetCount)(int *count);
+TINY_CUDA_CODES (*cuDeviceGetCount2)(int *count);
+TINY_CUDA_CODES (*cuGetProcAddress) ( const char* symbol, void** pfn, int  cudaVersion, uint64_t flags );
+TINY_CUDA_CODES (*cudaMemcpyFromArray)  ( void* dst, const cudaArray_t src, size_t wOffset, size_t hOffset, size_t count, unsigned int kind );
+
+#define LOAD_CUDA_FUNCTION(name, version) \
+  name = reinterpret_cast<decltype(name)>(dlsym(cuda_lib , #name version)); \
+  if (!name) cout << "Error:" << #name <<  " not found in CUDA library" << endl
+
+// cuda runtime (cudart.so)
+TINY_CUDA_CODES (*cudaGraphicsGLRegisterImage)( cudaGraphicsResource** resource, uint64_t image, int target, unsigned int  flags );
+//Returns the requested driver API function pointer.
+//see also https://forums.developer.nvidia.com/t/some-questions-about-cugetprocaddress/191749
+TINY_CUDA_CODES (*cudaGetDriverEntryPoint) ( const char* symbol, void** funcPtr, unsigned long long flags );
+TINY_CUDA_CODES (*cudaGraphicsMapResources) (	int 	count, cudaGraphicsResource* resources, cudaStream_t stream );
+TINY_CUDA_CODES (*cudaGraphicsSubResourceGetMappedArray) ( cudaArray_t* array, cudaGraphicsResource_t resource, unsigned int  arrayIndex, unsigned int  mipLevel );
+TINY_CUDA_CODES (*cudaGraphicsUnmapResources) (	int 	count, cudaGraphicsResource_t * 	resources, cudaStream_t 	stream );
+
+#define LOAD_CUDART_FUNCTION(name, version) \
+  name = reinterpret_cast<decltype(name)>(dlsym(cudart_lib , #name version)); \
+  if (!name) cout << "Error:" << #name <<  " not found in CUDA library" << endl
 
 int main(int argc, char* argv[])
 {
-cout << "hello cuda world" << endl;
+    const char* cuda_lib_path = DYNAMIC_CUDA_PATH;
+    void* cuda_lib = dlopen(cuda_lib_path, RTLD_NOW);
+    if (!cuda_lib) {
+        cout << "Unable to load library " << cuda_lib_path << endl << dlerror() << endl;
+        return false;
+    }
+
+    void* cudart_lib = dlopen(DYNAMIC_CUDART_PATH, RTLD_NOW);
+    if (!cudart_lib) {
+        cout << "Unable to load library " << DYNAMIC_CUDART_PATH << endl << dlerror() << endl;
+        return false;
+    }
+
+    cout << "hello cuda world" << endl;
+
+    LOAD_CUDA_FUNCTION(cuDriverGetVersion, "");
+    LOAD_CUDA_FUNCTION(cuInit, "");
+    LOAD_CUDA_FUNCTION(cuDeviceGetCount, "");
+    LOAD_CUDA_FUNCTION(cuGetProcAddress, "");
+        
+    LOAD_CUDART_FUNCTION(cudaGetDriverEntryPoint, "");
+    LOAD_CUDART_FUNCTION(cudaGraphicsGLRegisterImage, "");
+    LOAD_CUDART_FUNCTION(cudaGraphicsMapResources, "");
+    LOAD_CUDART_FUNCTION(cudaGraphicsSubResourceGetMappedArray, "");
+    LOAD_CUDART_FUNCTION(cudaMemcpyFromArray, "");
+    LOAD_CUDART_FUNCTION(cudaGraphicsUnmapResources, "");
+    
+    auto result = cuInit(0);
+    int cuda_driver_version;
+    result = cuDriverGetVersion(&cuda_driver_version);
+    cout << "CUDA driver version:" << cuda_driver_version << endl;
+    int device_count=0;
+    result = cuDeviceGetCount(&device_count);
+    cout << "CUDA device count:" << device_count << endl;
+
+
+    result = cuGetProcAddress("cuDeviceGetCount", (void**)&cuDeviceGetCount2, cuda_driver_version, CU_GET_PROC_ADDRESS_DEFAULT);
+    if (CUDA_SUCCESS != result)
+    {
+        cout << "cuDeviceGetCount not found" << endl;
+    }
 }
